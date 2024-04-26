3c52f9f9e8ad016ef48d8667a55f0c818badfbf7
Mad8dev <grandprodev@gmail.com>
2024-04-26 16:45:27 +0500

    ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,84 +12,74 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent implements OnInit{
 
-  info: any;
+
+  last:any
+  info: any[] = [];
   currentPage: number = 1;
   totalPages: number = 0;
   pageSize: number = 10;
   hasChecker: string = 'all';
   filterTitle: string = '';
-  sortOrder: 'asc' | 'desc' = 'asc';
+  sortOrder: "" | "asc" | "desc" | string = "";
 
- constructor(private http:HttpClient){
- }
+  constructor(private http: HttpClient) { }
 
- ngOnInit(): void {
-   this.getAllData()
- }
- getAllData(){
-
-  let queryParams= new HttpParams()
-
-  .set('page', this.currentPage.toString())
-  .set('page_size', this.pageSize.toString())
- .set('sort_by', 'id')  
-    .set('sort_order', this.sortOrder); 
-
-     
-
-  if (this.hasChecker !== 'all') {
-    queryParams = queryParams.set('has_checker', this.hasChecker);
+  ngOnInit(): void {
+    this.getAllData();
   }
 
-  if (this.filterTitle) {
-    queryParams = queryParams.set('title', this.filterTitle);
+  getAllData() {
+    let queryParams = new HttpParams()
+      .set('page', this.currentPage.toString())
+      .set('page_size', this.pageSize.toString())
+
+    if (this.hasChecker !== 'all') {
+      queryParams = queryParams.set('has_checker', this.hasChecker);
+    }
+
+    if (this.filterTitle) {
+      queryParams = queryParams.set('title', this.filterTitle);
+    }
+
+
+    if (this.sortOrder) {
+      queryParams = queryParams.set('ordering', this.sortOrder);
+    }
+
+    this.http.get<any>('https://kep.uz/api/problems', { params: queryParams }).subscribe((res) => {
+      this.info = res.data;
+      this.totalPages = Math.ceil(res.total / this.pageSize);
+      console.log(this.info);
+    });
   }
 
-  if (this.sortOrder) {
-    queryParams = queryParams.set('sort_order', this.sortOrder);
+  onPageChange(pageNumber: number) {
+    if (pageNumber < 1 || pageNumber > this.totalPages) {
+      return; 
+    }
+    this.currentPage = pageNumber;
+    this.getAllData();
   }
-  
-  
 
-  this.http.get<any>('https://kep.uz/api/problems', { params: queryParams }).subscribe((res) => {
-    this.info = res.data;
-    this.totalPages = Math.ceil(res.total / this.pageSize);
-    console.log(this.info);
-  });
-
-}
-
-onPageChange(pageNumber: number) {
-  if (pageNumber < 1 || pageNumber > this.totalPages) {
-    return; 
-  }
-  this.currentPage = pageNumber;
-  this.getAllData();
-}
-
-onPageSizeChange() {
-  this.currentPage = 1; 
-  this.getAllData();
-}
-
-onHasCheckerChange() {
-  this.currentPage = 1;
-  this.getAllData();
-}
-
-applyFilters() {
-  this.currentPage = 1; 
-  this.getAllData();
-}
-
-
-toggleSortOrder(order: 'asc' | 'desc') {
-  this.sortOrder = order;
-  this.getAllData();
-  if (this.sortOrder === 'desc') {
-    this.currentPage = this.totalPages; 
-  } else {
+  onPageSizeChange() {
     this.currentPage = 1; 
+    this.getAllData();
   }
-}
+
+  onHasCheckerChange() {
+    this.currentPage = 1;
+    this.getAllData();
+  }
+
+  applyFilters() {
+    this.currentPage = 1; 
+    this.getAllData();
+  }
+
+
+  sortBy(field: string) {
+    this.sortOrder = field;
+    this.getAllData();
+  }
+
 }
