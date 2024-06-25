240bc4d95539740e170acf58093224dea3a0ecee
Mad8dev <grandprodev@gmail.com>
2024-06-25 16:46:45 +0500

    intigrations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,61 +3,37 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {  NgxSpinnerService } from 'ngx-spinner';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HttpClientModule,FormsModule,CommonModule ],
+  imports: [RouterOutlet, HttpClientModule, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit{
-
-  color = 'primary';
-  mode = 'indeterminate';
-  value = 50;
-  displayProgressSpinner = false;
-  spinnerWithoutBackdrop = false;
-
-  last:any
+export class AppComponent implements OnInit {
   info: any[] = [];
   currentPage: number = 1;
   totalPages: number = 0;
   pageSize: number = 10;
   hasChecker: string = 'all';
   filterTitle: string = '';
-  sortOrder: "" | "asc" | "desc" | string = "";
+  sortOrder: '' | 'asc' | 'desc' | string = '';
   filterTimeout: any;
 
-  isLoading:boolean=false
-  
-  constructor(private http: HttpClient, private spinner: NgxSpinnerService) { }
+  isLoading: boolean = false;
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getAllData();
   }
 
-  showProgressSpinner = () => {
-    this.displayProgressSpinner = true;
-    setTimeout(() => {
-      this.displayProgressSpinner = false;
-    }, 3000);
-  };
-  showSpinnerWithoutBackdrop = () => {
-    this.spinnerWithoutBackdrop = true;
-    setTimeout(() => {
-      this.spinnerWithoutBackdrop = false;
-    }, 30000);
-  };
-
   getAllData() {
-    this.isLoading =true
+    this.isLoading = true;
     let queryParams = new HttpParams()
       .set('page', this.currentPage.toString())
-      .set('page_size', this.pageSize.toString())
+      .set('page_size', this.pageSize.toString());
 
     if (this.hasChecker !== 'all') {
       queryParams = queryParams.set('has_checker', this.hasChecker);
@@ -67,33 +43,31 @@ export class AppComponent implements OnInit{
       queryParams = queryParams.set('title', this.filterTitle);
     }
 
-
     if (this.sortOrder) {
       queryParams = queryParams.set('ordering', this.sortOrder);
     }
 
-    this.http.get<any>('https://kep.uz/api/problems', { params: queryParams }).subscribe((res) => {
-      this.info = res.data;
+    this.http
+      .get<any>('https://kep.uz/api/problems', { params: queryParams })
+      .subscribe((res) => {
+        this.info = res.data;
 
+        this.isLoading = false;
 
-        this.isLoading=false
-
-
-
-      this.totalPages = Math.ceil(res.total / this.pageSize);
-    });
+        this.totalPages = Math.ceil(res.total / this.pageSize);
+      });
   }
 
   onPageChange(pageNumber: number) {
     if (pageNumber < 1 || pageNumber > this.totalPages) {
-      return; 
+      return;
     }
     this.currentPage = pageNumber;
     this.getAllData();
   }
 
   onPageSizeChange() {
-    this.currentPage = 1; 
+    this.currentPage = 1;
     this.getAllData();
   }
 
@@ -103,19 +77,17 @@ export class AppComponent implements OnInit{
   }
 
   applyFilters() {
-  this.currentPage = 1;
+    this.currentPage = 1;
 
-  clearTimeout(this.filterTimeout);
+    clearTimeout(this.filterTimeout);
 
-  this.filterTimeout = setTimeout(() => {
-    this.getAllData();
-  }, 2000); 
+    this.filterTimeout = setTimeout(() => {
+      this.getAllData();
+    }, 2000);
   }
-
 
   sortBy(column: string) {
     this.sortOrder = this.sortOrder === column ? '-' + column : column;
     this.getAllData();
-}
-
+  }
 }
